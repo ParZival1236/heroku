51b1d78e7a8e1aab89feb0c14f0e53112d3ece0c
ParZival1236 <71388837+ParZival1236@users.noreply.github.com>
2022-01-25 13:35:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,110 +1,112 @@
-const api_url = "http://localhost:8080/user"
+api_url = "https://esdnode.herokuapp.com/user";
 
 function loadData(records = []) {
-	var table_data = "";
-	for(let i=0; i<records.length; i++) {
-		table_data += `<tr>`;
-		table_data += `<td>${records[i].name}</td>`;
-		table_data += `<td>${records[i].age}</td>`;
-		table_data += `<td>${records[i].city}</td>`;
-		table_data += `<td>`;
-		table_data += `<a href="edit.html?id=${records[i]._id}"><button class="btn btn-primary">Edit</button></a>`;
-		table_data += '&nbsp;&nbsp;';
-		table_data += `<button class="btn btn-danger" onclick=deleteData('${records[i]._id}')>Delete</button>`;
-		table_data += `</td>`;
-		table_data += `</tr>`;
-	}
-	//console.log(table_data);
-	document.getElementById("tbody").innerHTML = table_data;
+  console.log(records);
+  table_data = ``;
+  for (let i = 0; i < records.length; i++) {
+    // console.log(records[i].name);
+    table_data += `<tr>`;
+    table_data += `<td>${i + 1}</td>`;
+    table_data += `<td>${records[i].name}</td>`;
+    table_data += `<td>${records[i].age}</td>`;
+    table_data += `<td>${records[i].city}</td>`;
+    table_data += `<td>
+    <a href='edit.html?id=${records[i]._id}'><button>edit</button></a> | 
+    <button onclick='deleteData("${records[i]._id}","${records[i].name}");'>delete</button>
+    </td>`;
+    table_data += `</tr>`;
+  }
+
+  document.getElementById("table_data").innerHTML = table_data;
 }
 
 function getData() {
-	fetch(api_url)
-	.then((response) => response.json())
-	.then((data) => { 
-		console.table(data); 
-		loadData(data);
-	});
+  fetch(api_url)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      loadData(data);
+    });
 }
-
-
-function getDataById(id) {
-	fetch(`${api_url}/${id}`)
-	.then((response) => response.json())
-	.then((data) => { 
-	
-		console.log(data);
-		document.getElementById("id").value = data._id;
-		document.getElementById("name").value = data.name;
-		document.getElementById("age").value = data.age;
-		document.getElementById("city").value = data.city;
-	})
-}
-
 
 function postData() {
-	var name = document.getElementById("name").value;
-	var age = document.getElementById("age").value;
-	var city = document.getElementById("city").value;
-	
-	data = {name: name, age: age, city: city};
-	
-	fetch(api_url, {
-		method: "POST",
-		headers: {
-		  'Accept': 'application/json',
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	})
-	.then((response) => response.json())
-	.then((data) => { 
-		console.log(data); 
-		window.location.href = "index.html";
-	})
-}	
+  // console.log(`function called`);
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var city = document.getElementById("city").value;
 
+  data = { name: name, age: age, city: city };
+  // console.log(data);
 
-function putData() {
-	
-	var _id = document.getElementById("id").value;
-	var name = document.getElementById("name").value;
-	var age = document.getElementById("age").value;
-	var city = document.getElementById("city").value;
-	
-	data = {_id: _id, name: name, age: age, city: city};
-	
-	fetch(api_url, {
-		method: "PUT",
-		headers: {
-		  'Accept': 'application/json',
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	})
-	.then((response) => response.json())
-	.then((data) => { 
-		console.table(data);
-		window.location.href = "index.html";
-	})
+  fetch(api_url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      window.location.href = "index.html";
+    });
 }
 
+function deleteData(id, name) {
+  // alert(`name: ${name}, id: ${id}`);
+  user_input = confirm(`Are you sure you want to delete ${name} record?`);
+  if (user_input) {
+    fetch(api_url, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        window.location.href = "index.html";
+      });
+  }
+}
 
-function deleteData(id) {
-	user_input = confirm("Are you sure you want to delete this record?");
-	if(user_input) {
-		fetch(api_url, {
-			method: "DELETE",
-			headers: {
-			  'Accept': 'application/json',
-			  'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({"_id": id})
-		})
-		.then((response) => response.json())
-		.then((data) => { 
-			console.log(data); 
-			window.location.reload();
-		})
-	}
+function getDataById(id) {
+  // alert(`${api_url}/${id}`);
+  fetch(`${api_url}/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("id").value = data[0]._id;
+      document.getElementById("name").value = data[0].name;
+      document.getElementById("age").value = data[0].age;
+      document.getElementById("city").value = data[0].city;
+    });
+}
+
+function putData() {
+  var id = document.getElementById("id").value;
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var city = document.getElementById("city").value;
+
+  data = { id: id, name: name, age: age, city: city };
+
+//   console.log(data);
+
+  fetch(api_url, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.table(data);
+      window.location.href = "index.html";
+    });
 }
